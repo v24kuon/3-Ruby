27f3ae364252bfd192f0f06930490156c934d419
v24kuon <v24kuong@gmail.com>
2020-05-05 01:02:34 +0900

    [3-6finish]

--- type.rb
+++ type.rb
@@ -12,3 +12,17 @@ Pi = 3.14
 puts Pi
 
 puts 100
+puts 100 + 5
+
+puts "Rubyを学習"
+
+name = "鈴木さん"
+puts "Hello" + name
+
+puts "こんにちは#{name}"
+
+names = ["Perl", "Ruby", "Python", "Swift"]
+puts names[1]
+
+tall = {:太郎=>185, :二郎=>170, :花子=>150}
+puts tall[:太郎]
