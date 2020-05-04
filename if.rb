e6a5835faee3297ce0233e2573e9d138ec7b29f8
v24kuon <v24kuong@gmail.com>
2020-05-05 02:23:39 +0900

    [3-7finish]

--- if.rb
+++ if.rb
@@ -1,9 +1,21 @@
 apple = 110
- 
+
 if apple >= 100
     puts "りんごの値段は100円以上です"
 end
-if apple <= 100　
+if apple <= 100
   　puts "りんごの値段は100円以下です"
 end
+tall = 180
+if tall >= 170 && tall <= 190
+    puts "身長は170以上190以下です。"
+end
+apple = "Yamagata"
 
+if apple == "Nagano"
+    puts "りんごの生産地は長野です"
+elsif apple == "Yamagata"
+    puts "りんごの生産地は山形です"
+else
+    puts "りんごの生産地は青森です"
+end
